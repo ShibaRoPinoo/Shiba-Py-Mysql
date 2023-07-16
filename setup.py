926a2d461824509874aa3cde3f2932c53bcb834f
RoPinoo <rodrigo@ducklifemedia.com>
2023-07-15 21:32:39 -0400

    v1.1.1

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    'Developer Status :: 5 - Production/Stable'
-    'Intented Audience :: Developers'
-    'Operating System :: Microsoft :: Windows :: Windows 10 :: Windows 11'
-    'License :: MIT License'
-    'Programming Language :: Python : 3'
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Topic :: Database',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
 setup(
     name='shiba_mysql',
-    version='1.0.0',
+    version='1.1.1',
     description='A library to interact with MySQL',
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
     author='Rodrigo Pino',
     license='MIT',
     classifiers=classifiers,
-    keywords='mysql',
+    keywords=['mysql', 'database', 'SQL', 'data access', 'ORM'],
     author_email='ro.pinoo18@gmail.com',
     url='https://github.com/ShibaRoPinoo/Shiba-Py-Mysql',
-    packages=['shiba'],
+    packages=find_packages(),
     install_requires=['pymysql'],
 )
